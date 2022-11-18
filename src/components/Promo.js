1280750263a0b32e93be678491b7d17d49a2c335
Mohamed Murtada <Mohamed.Murtada1@moe.gov.ae>
2022-11-18 07:39:19 +0400

    add components and import them

---
+++ src/components/Promo.js
@@ -0,0 +1,16 @@
+function Promo() {
+    return (
+        <div className="promo-section">
+            <div>
+                <h1>Dont miss this deal!</h1>
+            </div>
+            <div>
+                <h2>Subscribe to my neletter and get all the shop items at 50% off !</h2>
+            </div>
+        </div>
+        
+    );
+};
+
+
+export default Promo;
